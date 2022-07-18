98ab69349f2470cc28cd71702a78982500ffcfd9
Taner Saydam <tanersaydam@gmail.com>
2022-07-18 06:33:09 +0300

    Add project files.

---
+++ Core/Models/UserApp.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Core.Models
+{
+    public class UserApp : IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,24 @@
+﻿namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool ısShow)
+        {
+            Errors = errors;
+            IsShow = ısShow;
+        }
+    }
+}
